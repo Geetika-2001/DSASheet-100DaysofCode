191f1dc3d6c09c96288b4f7a5413261708d2ebe5
Geetika-2001 <geetikakaushik2001@gmail.com>
2022-07-04 11:11:18 +0530

    100 Days of Code

---
+++ Arrays/Q1.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Q1 {
+    //Given an array (or string), the task is to reverse the array/string.
+    public static void main (String[] args) {
+        
+        int[] arr= {1 , 2 , 3, 4 ,5 , 6};
+        int n = arr.length;
+        int start =0;
+        int end = n-1;
+
+        int temp;
+
+        //swap
+        while(start<end) {
+            temp= arr[start];
+            arr[start] = arr[end];
+            arr[end]= temp;
+
+            start++;
+            end--;
+        }
+        System.out.println("Reversed Array is:");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
